44a16c292718ff935456cc712444d5d67a8a6540
NathanCamathias <nathan.camathias@gmail.com>
2024-05-22 17:27:17 +0200

    Update sketch.js

--- Esercizio_3A/orologio analogioco/sketch.js
+++ Esercizio_3A/orologio analogioco/sketch.js
@@ -12,30 +12,30 @@ function draw() {
 
 	translate(width/2, height/2)
 
-	stroke(0, 50)
-	line(-200, 0, 200, 0)
-	line(0, -200 , 0, 200)
 
 	noStroke()
 	fill(0)
 
 	push()
+	fill(50)
 	rotate(TAU / 60 * (hour() %12)+ TAU /12 / 60 * minute())
 	rect(-8, 25, 16, -140)
 	pop()
 
 	push()
+	fill(30)
 	rotate(TAU / 60 * minute())
 	rect(-10, 25, 20, -170)
 	pop()
 
 	push()
+	fill(10)
 	rotate(TAU / 60 * second())
 	rect(-2, 25, 4, -180)
 	circle(0,-160,24)
 	pop()
 
 	fill(255)
-	circle(0, 0, 12)
+	circle(0, 0, 17)
 }
 
